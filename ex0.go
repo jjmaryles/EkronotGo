44a0f3fe16e802e21fbcedd42e4be48b1d8bf33b
JJ Maryles <jjmaryles1@gmail.com>
2022-02-28 15:39:41 +0200

    doesnt work yet, dont mess it up

--- ex0.go
+++ ex0.go
@@ -6,25 +6,32 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
+var outfileName string //global variable as outfile name
+
 func main() {
-	fmt.Println("Enter file path: ")
+	//get filepath input
 	var path string
+	fmt.Println("Enter file path: ")
 	fmt.Scanln(&path)
 
-	var vmFiles []string = GetVmFiles(path)
+	//create list of ".vm" files
+	vmFiles := GetVmFiles(path)
 
-	var outname string = OutfileName(path)
-	outfile, err1 := os.Create(outname)
+	//create output file to write to
+	outfileName = filepath.Base(path) + ".asm"
+	outfile, err1 := os.Create(outfileName)
 	if err1 != nil {
 		log.Fatal(err1)
 	}
 	defer outfile.Close()
 
 	var name string
-	var line string
 	for _, file := range vmFiles {
+		//write filename without .vm
 		name = filepath.Base(file)
 		outfile.WriteString(name[:len(name)-3] + "\n")
 
@@ -34,10 +41,19 @@ func main() {
 		}
 		defer current.Close()
 
+		//read the file
 		scanner := bufio.NewScanner(current)
+		//for each line, check if it is "buy" or "cell" and call corresponding "Handle" functions
 		for scanner.Scan() {
-			line = scanner.Text()
-			println(line)
+			line := strings.Split(scanner.Text(), " ")
+			amount, _ := strconv.Atoi(line[2])
+			price, _ := strconv.ParseFloat(line[3], 32)
+			if line[0] == "buy" {
+				HandleBuy(line[1], amount, price)
+			}
+			if line[0] == "cell" {
+				HandleCell(line[1], amount, price)
+			}
 		}
 
 		if err2 := scanner.Err(); err2 != nil {
@@ -46,6 +62,7 @@ func main() {
 	}
 }
 
+// GetVmFiles function returns of list of ".vm" files in a specified directory
 func GetVmFiles(folder string) []string {
 	var files []string
 
@@ -68,24 +85,22 @@ func GetVmFiles(folder string) []string {
 	return res
 }
 
+// HandleBuy Handle function for "buy"
 func HandleBuy(ProductName string, Amount int, Price float64) {
-	writefile, err1 := os.Open("ex0.asm")
+	writefile, err1 := os.Open(outfileName)
 	if err1 != nil {
 		log.Fatal(err1)
 	}
 	writefile.WriteString("### BUY " + ProductName + " ###\n")
-	writefile.WriteString(fmt.Sprintf("%f", float64(Amount)*Price) + "/n")
+	writefile.WriteString(strconv.FormatFloat(float64(Amount)*Price, 'E', -1, 64) + "/n")
 }
 
+//Handle function for "cell"
 func HandleCell(ProductName string, Amount int, Price float64) {
-	writefile, err1 := os.Open("ex0.asm")
+	writefile, err1 := os.Open(outfileName)
 	if err1 != nil {
 		log.Fatal(err1)
 	}
 	writefile.WriteString("### CELL " + ProductName + " ###\n")
-	writefile.WriteString(fmt.Sprintf("%f", float64(Amount)*Price) + "/n")
-}
-
-func OutfileName(path string) string {
-	return filepath.Base(path) + ".asm"
+	writefile.WriteString(strconv.FormatFloat(float64(Amount)*Price, 'E', -1, 64) + "/n")
 }
