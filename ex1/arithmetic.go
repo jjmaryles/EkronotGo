2dbb8659efb44bffe18db8903d3d7687cdd38c21
yehoshua18 <gruenspe@g.jct.ac.il>
2022-03-19 21:33:41 +0200

    eq, lt, gt

--- ex1/arithmetic.go
+++ ex1/arithmetic.go
@@ -1,28 +1,29 @@
 package main
 import (
 	"strings"
+	"fmt"
 )
 func writeArithmetic(line []string) {
 	firstWord := line[0]
 	var res []string
 	var strTemp string
+	count := 0
 	switch firstWord{
 	case "add","sub","and","or":
 		if firstWord == "add"{
-			res[0] = "D=D+M"
+			strTemp = "D=D+M"
 		} else if firstWord == "sub"{
-			res[0] = "D=M-D"
+			strTemp = "D=M-D"
 		} else if firstWord == "and"{
-			res[0] = "D=D&M"
+			strTemp = "D=D&M"
 		} else if firstWord == "or"{
-			res[0] = "D=D|M"
+			strTemp = "D=D|M"
 		}
 		res = append(res, "@SP\n")
 		res = append(res, "M=M-1\n")
 		res = append(res, "A=M\n")
 		res = append(res, "D=M\n")
 		res = append(res, "A=A-1\n")
-		strTemp = strings.Join(res, "\n")
 		res = append(res,  strTemp)
 		res = append(res, "M=D\n")
 		res = append(res, "D=A+1\n")
@@ -31,18 +32,56 @@ func writeArithmetic(line []string) {
 
 	case "neg", "not":
 		if firstWord == "neg" {
-			res = append(res, "M=-M")
+			strTemp = "M=-M"
 		}else{
-			res = append(res,"M!=M")
+			strTemp = "M!=M"
 		}
 		res = append(res, "@SP\n")
 		res = append(res, "M=M-1\n")
 		res = append(res, "A=M\n")
-		strTemp = strings.Join(res, "\n")
 		res = append(res,  strTemp)
 		res = append(res, "D=A+1\n")
 		res = append(res, "@SP\n")
 		res = append(res, "M=D\n")
+
+	case "eq", "lt", "gt":
+		if firstWord == "eq"{
+			strTemp = "D;JEQ"
+		} else if firstWord == "lt"{
+			strTemp = "D;JLT"
+		} else if firstWord == "gt"{
+			strTemp = "D;JGT"
+		}
+		res = append(res, "@SP\n")
+		res = append(res, "M=M-1\n")
+		res = append(res, "A=M\n")
+		res = append(res, "D=M\n")
+		res = append(res, "A=A-1\n")
+		res = append(res, "D=M-D\n")
+		res = append(res, fmt.Sprint("@EQ%d\n", count))
+		res = append(res, strTemp)
+		res = append(res, "@0\n")
+		res = append(res, "D=-A\n")
+		res = append(res, "@SP\n")
+		res = append(res, "A=M\n")
+		res = append(res, "M=D\n")
+		res = append(res, "@SP\n")
+		res = append(res, "M=M+1\n")
+		res = append(res, fmt.Sprint("@doneEQ%d\n", count))
+		res = append(res, "0;JMP\n")
+		res = append(res, fmt.Sprint("EQ%d\n", count))
+		res = append(res, "@1\n")
+		res = append(res, "D=A\n")
+		res = append(res, "@SP\n")
+		res = append(res, "A=M\n")
+		res = append(res, "M=D\n")
+		res = append(res, "@SP\n")
+		res = append(res, "M=M+1\n")
+		res = append(res, fmt.Sprint("doneEQ%d\n", count))
+
+		count = count + 1
 	}
+
+
 }
 
