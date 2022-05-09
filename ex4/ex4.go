907af5fda19fba96b790f5b8abd061b1216e89ef
yehoshua18 <gruenspe@g.jct.ac.il>
2022-05-09 18:11:49 +0300

    ex4 - reads jack files to xml

---
+++ ex4/ex4.go
@@ -0,0 +1,75 @@
+package main
+/**
+The following program imitates the syntax analyser part of the compiler
+This is done in two steps:
+1. Tokenizing
+2. Parsing
+ */
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	//get filepath input
+	var path string
+	fmt.Println("Enter file path: ")
+	fmt.Scanln(&path)
+
+	//create list of ".jack" files
+	jackFiles := GetJackFiles(path)
+
+	//open jackFiles to read from
+	var toWrite []string
+
+	//for each jack file
+	for _, file := range jackFiles {
+		current, err2 := os.Open(file)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		defer current.Close()
+
+		//create a corresponding xml file for translation
+		currentOut := filepath.Base(NoSuffix(file)) + ".xml"
+		out, err1 := os.Create(currentOut)
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+
+		defer out.Close()
+
+	}
+
+}
+
+// NoSuffix function returns file name without type
+func NoSuffix(fileName string) string {
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
+}
+
+// GetJackFiles function returns of list of ".jack" files in a specified directory
+func GetJackFiles(folder string) []string {
+	var files []string
+
+	root := folder
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	var res []string
+	for _, file := range files {
+		if file[len(file)-3:] == ".jack" {
+			res = append(res, file)
+		}
+	}
+	return res
+}
